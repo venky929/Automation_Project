70664a7578fda5d9a05e61deff01623ee599108d
root <root@ip-172-31-88-83.ec2.internal>
2021-08-30 16:05:28 +0000

    Edited the script

--- automation.sh
+++ automation.sh
@@ -15,7 +15,7 @@ fi
 
 #check if apache2 is running
 running=$(systemctl status apache2 | grep active | awk '{print $3}' | tr -d '()')
-if [[running != ${running} ]]; then
+if [[ running != ${running} ]]; then
 	systemctl start apache2
 fi
 
@@ -27,7 +27,7 @@ fi
 
 #creating file
 
-timestamp=$(date '%d%m%Y-%H%M%S')
+timestamp=$(date '+%d%m%Y-%H%M%S')
 
 #create tar archive
 
@@ -36,5 +36,5 @@ tar -cf /tmp/${name}-httpd-logs-${timestamp}.tar *.log
 
 #copy logs to s3 bucket
 if [[ -f /tmp/${name}-httpd-logs-${timestamp}.tar ]]; then
-	aws cp /tmp/${name}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${name}-httpd-logs-${timestamp}.tar
+	aws s3 cp /tmp/${name}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${name}-httpd-logs-${timestamp}.tar
 fi
